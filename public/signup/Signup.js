81cb04ff183711c8926f622f5e2f8c8ddab28c57
MohammadAyubAnsari <119043948+MohammadAyubAnsari@users.noreply.github.com>
2023-08-22 21:05:30 +0530

    Update Signup.js

--- public/signup/Signup.js
+++ public/signup/Signup.js
@@ -14,7 +14,7 @@ async function signup(event) {
     document.getElementById("phno").value = "";
 
     const response = await axios.post(
-      "http://43.205.212.2:3001/user/signup",
+      "http://35.154.205.244:3000/user/signup",
       signupDetails
     );
     if (response.status === 201) {
